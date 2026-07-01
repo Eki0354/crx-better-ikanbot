e8d688f61ebf2fcc250fc15a978718047c9f68eb
Eki <ichinoseeki@outlook.com>
2026-07-01 16:12:40 +0800

    chore: 删除冗余代码.

--- entrypoints/inject-ikanbot-main-world.ts
+++ entrypoints/inject-ikanbot-main-world.ts
@@ -2,15 +2,6 @@
 declare var A_Store: any;
 
 export default defineUnlistedScript(() => {
-  // 这里的代码可以访问原页面的 window 变量
-  console.log(
-    "Inject script in MAIN world, page title:",
-    window.document.title,
-  );
-  console.log("Page variable v_tks:", (window as any).v_tks);
-
-  localStorage.setItem("token", (window as any).v_tks);
-
   try {
     A_Store.isCooldown = () => true; // 屏蔽广告判断逻辑
   } catch (e) {
